80f7cc765f9bb52d0a06b509fd4b58dfc4abff60
levspb666 <levspb666@yandex.ru>
2019-04-28 10:11:03 +0300

    1 1 other activity

--- app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.javaops.android.tamagotchi;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+        startActivity(intent);
     }
 }
