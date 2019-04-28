c593c749db1f59457f2e9a770bdf7748a32cad4d
levspb666 <levspb666@yandex.ru>
2019-04-28 10:11:47 +0300

    1 2 animation

--- app/src/main/java/ru/javaops/android/tamagotchi/OtherActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/OtherActivity.java
@@ -3,23 +3,34 @@ package ru.javaops.android.tamagotchi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OtherActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
-        ImageButton home = findViewById(R.id.button_home);
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        initViews();
+        final Animation helloAnimation = AnimationUtils.loadAnimation(this, R.anim.hello);
+        textView.startAnimation(helloAnimation);
+    }
+
+    private void initViews() {
+        textView = findViewById(R.id.title_hello);
+        findViewById(R.id.button_home)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
