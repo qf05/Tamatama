d4121c97af58861ab84a4b49e8b8c38db8a43611
levspb666 <levspb666@yandex.ru>
2019-04-28 10:12:17 +0300

    1 3 translate animation

---
+++ app/src/main/java/ru/javaops/android/tamagotchi/utils/ViewHelper.java
@@ -0,0 +1,20 @@
+package ru.javaops.android.tamagotchi.utils;
+
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+public class ViewHelper {
+
+    public static void executeAfterViewDrawing(final View v, final Runnable cb) {
+        ViewTreeObserver vto = v.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                cb.run();
+                ViewTreeObserver obs = v.getViewTreeObserver();
+                obs.removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+}
