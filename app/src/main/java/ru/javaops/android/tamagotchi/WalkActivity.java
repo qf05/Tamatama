20c5ad91cbfb2994af0cbc003577b7a102f7c9b4
levspb666 <levspb666@yandex.ru>
2019-04-30 13:45:56 +0300

    2 4 create pet dialog

--- app/src/main/java/ru/javaops/android/tamagotchi/WalkActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/WalkActivity.java
@@ -18,6 +18,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ru.javaops.android.tamagotchi.enums.PetsType;
+import ru.javaops.android.tamagotchi.model.Pet;
+import ru.javaops.android.tamagotchi.utils.PetUtils;
 import ru.javaops.android.tamagotchi.utils.SoundHelper;
 import ru.javaops.android.tamagotchi.utils.ViewHelper;
 
@@ -26,7 +28,7 @@ import static android.view.View.TRANSLATION_Y;
 
 @SuppressLint("ClickableViewAccessibility")
 public class WalkActivity extends AppCompatActivity {
-    public static final String INTENT_PET_TYPE = "pet_type";
+
     private static final String SAVE_COUNT = "save_count";
 
     private static final double DOG_VIEW_MAGNIFICATION = 1.8;
@@ -105,7 +107,8 @@ public class WalkActivity extends AppCompatActivity {
     private void initViews() {
         countTextView = findViewById(R.id.counter);
         petView = findViewById(R.id.image_pet);
-        final PetsType petsType = PetsType.valueOf(getIntent().getStringExtra(INTENT_PET_TYPE));
+        final Pet pet = PetUtils.getSelectedPet();
+        final PetsType petsType = pet.getPetsType();
         petView.setImageResource(petsType.getDrawableResource());
         if (PetsType.DOG == petsType) {
             ViewGroup.LayoutParams params = petView.getLayoutParams();
--- app/src/main/java/ru/javaops/android/tamagotchi/utils/ViewHelper.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/utils/ViewHelper.java
@@ -6,6 +6,9 @@ import android.view.ViewTreeObserver;
 
 public class ViewHelper {
 
+    private ViewHelper() {
+    }
+
     public static void executeAfterViewDrawing(final View v, final Runnable cb) {
         ViewTreeObserver vto = v.getViewTreeObserver();
         vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
