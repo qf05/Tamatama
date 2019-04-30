6bfef7c195a087eba429c1dd9d85547dca0c275d
levspb666 <levspb666@yandex.ru>
2019-04-30 13:33:39 +0300

    HW 1 1 buttons

--- app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
@@ -3,6 +3,7 @@ package ru.javaops.android.tamagotchi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
     public void onButtonClick(View view) {
         Intent intent = new Intent(MainActivity.this, OtherActivity.class);
         startActivity(intent);
+    }
+
+    private void initViews() {
+        Button cat = findViewById(R.id.button_cat);
+        Button dog = findViewById(R.id.button_dog);
+        Button cthulhu = findViewById(R.id.button_cthulhu);
+
+        final View.OnClickListener buttonListener = initButtonListener();
+        cat.setOnClickListener(buttonListener);
+        dog.setOnClickListener(buttonListener);
+        cthulhu.setOnClickListener(buttonListener);
+    }
+
+    private View.OnClickListener initButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
     }
 }
