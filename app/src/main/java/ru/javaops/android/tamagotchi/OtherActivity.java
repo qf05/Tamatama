d7abf826aa155a77f5c9ae3d1a295f9f1f2e36e7
levspb666 <levspb666@yandex.ru>
2019-04-30 13:32:59 +0300

    1 3 translate animation

--- app/src/main/java/ru/javaops/android/tamagotchi/OtherActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/OtherActivity.java
@@ -4,22 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ru.javaops.android.tamagotchi.utils.ViewHelper;
+
 public class OtherActivity extends AppCompatActivity {
+    private static final int ANIMATION_OFFSET = 4000;
+    private static final int ANIMATION_DURATION_MILLIS = 2000;
+    private static final int ANIMATION_REPEAT_THRESHOLD = 10;
 
     private TextView textView;
+    private Animation translateAnimation;
+    private Animation.AnimationListener animationListener;
+
+    private int countAnimationRepeat = 1;
+    private float height;
+    private float width;
+    private float yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
         initViews();
-        final Animation helloAnimation = AnimationUtils.loadAnimation(this, R.anim.hello);
-        textView.startAnimation(helloAnimation);
+        initAnimationListener();
+        initAnimation();
     }
 
     private void initViews() {
@@ -32,5 +48,60 @@ public class OtherActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
+    }
+
+    private void initAnimationListener() {
+        animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                countAnimationRepeat++;
+                if (countAnimationRepeat < ANIMATION_REPEAT_THRESHOLD) {
+                    if (countAnimationRepeat % 2 == 0) {
+                        yDelta = height - (height / countAnimationRepeat);
+                    }
+                    translateAnimation = new TranslateAnimation(width, width, yDelta, height);
+                    startAnimation();
+                } else {
+                    textView.setX(width + (int) textView.getX());
+                    textView.setY(height);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        };
+    }
+
+    private void initAnimation() {
+        final Animation helloAnimation = AnimationUtils.loadAnimation(this, R.anim.hello);
+        final AnimationSet animationSet = new AnimationSet(true);
+        final FrameLayout frameLayout = findViewById(R.id.layout);
+        ViewHelper.executeAfterViewDrawing(textView, new Runnable() {
+            @Override
+            public void run() {
+                height = frameLayout.getHeight() - textView.getHeight();
+                width = (frameLayout.getWidth() / 2) - (textView.getWidth() / 2) - (int) textView.getX();
+
+                translateAnimation = new TranslateAnimation(width, width, textView.getY(), height);
+                translateAnimation.setDuration(ANIMATION_DURATION_MILLIS);
+                translateAnimation.setStartOffset(ANIMATION_OFFSET);
+                translateAnimation.setAnimationListener(animationListener);
+
+                animationSet.addAnimation(translateAnimation);
+                animationSet.addAnimation(helloAnimation);
+                textView.startAnimation(animationSet);
+            }
+        });
+    }
+
+    private void startAnimation() {
+        translateAnimation.setDuration(ANIMATION_DURATION_MILLIS / countAnimationRepeat);
+        translateAnimation.setAnimationListener(animationListener);
+        textView.startAnimation(translateAnimation);
     }
 }
