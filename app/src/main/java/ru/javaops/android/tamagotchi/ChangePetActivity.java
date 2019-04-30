70b8e777c6093625a51ea2c7ea4920a30fd72625
levspb666 <levspb666@yandex.ru>
2019-04-30 13:47:48 +0300

    2 7 recycler view change pet

---
+++ app/src/main/java/ru/javaops/android/tamagotchi/ChangePetActivity.java
@@ -0,0 +1,56 @@
+package ru.javaops.android.tamagotchi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ru.javaops.android.tamagotchi.adapters.PetAdapter;
+import ru.javaops.android.tamagotchi.db.DataBase;
+import ru.javaops.android.tamagotchi.model.Pet;
+import ru.javaops.android.tamagotchi.utils.PrefsUtils;
+
+public class ChangePetActivity extends AppCompatActivity implements PetAdapter.ItemClickListener {
+
+    private PetAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pet);
+
+        initRecycler();
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        PrefsUtils.saveSelectedPetId(this, adapter.getItem(position).getId());
+        goBack(null);
+    }
+
+    public void goBack(View view) {
+        Intent intent = new Intent(ChangePetActivity.this, SettingsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initRecycler() {
+        final RecyclerView rv = findViewById(R.id.change_rv);
+        rv.setHasFixedSize(true);
+        final LinearLayoutManager llm = new LinearLayoutManager(ChangePetActivity.this);
+        rv.setLayoutManager(llm);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rv.getContext(),
+                llm.getOrientation());
+        rv.addItemDecoration(dividerItemDecoration);
+
+        final List<Pet> pets = DataBase.getAppDatabase(getApplicationContext()).petDao().getAll();
+        adapter = new PetAdapter(pets, this);
+        rv.setAdapter(adapter);
+    }
+}
